cd7aad72f709c8ad12fa3d796ed0bf4f0b763dab
Twurtlil-C <josuejacobdomingo@gmail.com>
2024-08-06 21:26:18 +1000

    Week 4 - Activity 9 Content Added
    - Added Player and Enemy health bars

---
+++ IGB190 Base Project/Assets/Scripts/UnitUI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitUI : MonoBehaviour
+{
+    public RectTransform container;
+    public Image healthBar;
+    public Vector3 offset = new Vector3(0, 2, 0);
+    private IDamageable trackedDamageable;
+    private Transform trackedTransform;
+
+    private void Awake()
+    {
+        trackedDamageable = GetComponentInParent<IDamageable>();
+        trackedTransform = transform.parent;
+    }
+
+    private void LateUpdate()
+    {
+        // Move health bar to the correct screen position
+        Vector3 world = trackedTransform.position + offset;
+        container.anchoredPosition = Camera.main.WorldToScreenPoint(world);
+
+        // Update amount of visible red health bar
+        healthBar.fillAmount = trackedDamageable.GetCurrentHealthPercent();
+    }
+}
